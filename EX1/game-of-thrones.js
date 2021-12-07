f2bac4189845a42d0806ba44dc5e6efce3cd43f7
SalwaRayan <90609792+SalwaRayan@users.noreply.github.com>
2021-12-07 11:08:05 +0100

    corrected

--- EX1/game-of-thrones.js
+++ EX1/game-of-thrones.js
@@ -1,20 +1,17 @@
 const express = require('express')
 const app = express()
 const axios = require("axios")
+
 const characters = require("./characters.json")
 
-const port = 5000
-
-app.listen(port, () => {
-  console.log('Server started on port: ' + port)
-})
-
-app.get('/game-of-thrones/json', (req, res) => {
+app.get('/json', (req, res) => {
   res.send(characters)
 })
 
-app.get('/game-of-thrones/url', (req, res) => {
+app.get('/url', (req, res) => {
   axios.get('https://thronesapi.com/api/v2/Characters')
   .then(response => res.json(response.data))
   .catch((error) => console.log(error))
 })
+
+module.export = app
